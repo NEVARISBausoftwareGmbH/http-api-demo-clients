97589fb45fe6cb26e9fa0277aff5cbdcddf94970
Stefan Wille <wist@bausoftware.com>
2022-06-13 08:12:39 +0200

    Stylesheet für den Langtext geändert.

--- LvViewer/FormattedTextTemplate.cs
+++ LvViewer/FormattedTextTemplate.cs
@@ -13,12 +13,10 @@ namespace Lv_Viewer
                 "<html>" +
                     "<head>" +
                         "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">" +
-                        "<style>" +
-                            "<body>" +
-                                "{ font-size: 9px; }" +
-                            "</body>" +
+                        "<style>" +                            
                             "al { background-color: rgb(255, 255, 23); }" +
                             "bl { background-color: rgb(13, 164, 13); }" +
+                            "p { font-size: 0.75em; font-family: Segoe UI; }" +
                         "</style>" +
                     "</head>" +
                   "</html>";
