4e9a66f3040337cb98c4f4335e01782c30934932
Andreas Horwath <hoan@bausoftware.com>
2023-02-01 11:36:20 +0100

    LV Viewer: Auflistung der Speicherort-Ordner

---
+++ LvViewer/SpeicherortOrdnerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Nevaris.Build.ClientApi;
+
+namespace Lv_Viewer;
+
+/// <summary>
+/// ViewItem für einen Ordner innerhalb eines Speicherorts (inklusive Wurzelordner).
+/// </summary>
+public class SpeicherortOrdnerViewModel
+{
+    public SpeicherortOrdnerViewModel(Speicherort speicherort, SpeicherortOrdner? ordner, string pfad)
+    {
+        Speicherort = speicherort;
+        Ordner = ordner;
+        Pfad = pfad;
+    }
+
+    /// <summary>
+    /// Der Speicherort.
+    /// </summary>
+    public Speicherort Speicherort { get; }
+    
+    /// <summary>
+    /// Der Ordner (null für die Wurzelebene).
+    /// </summary>
+    public SpeicherortOrdner? Ordner { get; }
+
+    /// <summary>
+    /// Der vollständige Pfad (Speicherort-Name + Ordner-Namen)
+    /// </summary>
+    public string Pfad { get; }
+}
